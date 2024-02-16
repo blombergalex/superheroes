8bb4fb510b5b8b58ecad128d2c36e44ade1b87c2
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-16 17:11:03 +0100

    install express to render static files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var http = require("http");
-var app = require("./app");
+const express = require("express");
+const app = express();
 
-http.createServer(app.handleRequest).listen(3000, function(error) {
-    if (error) {
-        console.log("Oops, something went wrong!", error)
-    } else {
-        console.log("Server listening on port 3000")
-    }
+const superApp = require("./app");
+
+app.use(express.static("public"));
+app.use(superApp.handleRequest);
+
+
+const server = app.listen(3000, () => {
+    console.log("Server listening on port 3000");
 });
 
