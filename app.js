9fa4a4327d01751ecec0c7343955e8b0cd4ea2f0
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-16 13:54:52 +0100

    complete routing for url queries

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var fs = require("fs");
 const renderContent = (path, response) => {
     fs.readFile(path, "utf8", function(error,data) { 
         if (error) {
+            console.log("Error reading file:", error);
             response.writeHead(404);
-            response.end("Oh no! File not found.");
-            response.write()
+            response.end("Oops! Looks like the Batcave is empty. The superhero file you're looking for seems to have flown off into the sunset. Try checking the coordinates again!");
         } else {
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(data);
@@ -17,7 +17,10 @@ const renderContent = (path, response) => {
 
 const handleRequest = (request, response) => {
 
-    var path = new URL (request.url, "http://localhost:3000").pathname;
+    var urlObject = new URL (request.url, "http://localhost:3000");
+    var path = urlObject.pathname;
+    var searchParameters = urlObject.searchParams;
+
     switch (path) {
         case "/":
         case "/home":
@@ -28,9 +31,15 @@ const handleRequest = (request, response) => {
             break;
         case "/general":
             renderContent("./general.html", response);
-        break;
-        case "/superhero":
-            renderContent("./superhero.html", response);
+            break;
+        case "/superheroes":
+            var superheroName = searchParameters.get("name");
+            console.log(superheroName);
+            if (superheroName) {
+                renderContent(`./heroes/${superheroName}.html`, response);
+            } else {
+                renderContent("./superheroes.html", response);
+            }
             break;
         default: 
             response.write("Oops! This route is not defined Here's a joke to entertain you instead: Why did Batman and Robin never fight crime on April 1st? ............................................. Because the Joker would take it too seriously!");
