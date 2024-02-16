976ff34c53530e28e12d8d7d32c44ab25dbf8b10
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-16 10:55:23 +0100

    home and about working

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var url = require("url");
 var fs = require("fs");
 
+
 const renderHTML = (path, response) => {
     fs.readFile(path, null, function(error,data) { //might replace null with utf8
         if (error) {
-            response.writeHead(404);
-            response.write("File not found");
+            res.writeHead(404);
+            res.write("Oh no! File not found.");
         } else {
             response.write(data);
         }
@@ -21,11 +22,14 @@ const handleRequest = (request, response) => {
         case "/":
             renderHTML("./index.html", response);
             break;
+        case "/home":
+            renderHTML("./index.html", response);
+            break;
         case "/about":
             renderHTML("./about.html", response);
             break;
         default: 
-            response.write("Oops something went wrong! Route not defined");
+            response.write("Oops something went wrong! Route not defined.");
             response.end();
     }
 }
