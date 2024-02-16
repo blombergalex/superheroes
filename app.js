4c2e288796cc50643a278c910a77df7ec4b6625d
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-16 12:39:08 +0100

    content fixes

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var url = require("url");
 var fs = require("fs");
 
 const renderContent = (path, response) => {
-    fs.readFile(path, "utf8", function(error,data) { //might replace null with utf8
+    fs.readFile(path, "utf8", function(error,data) { 
         if (error) {
             response.writeHead(404);
             response.end("Oh no! File not found.");
+            response.write()
         } else {
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(data);
@@ -26,13 +27,13 @@ const handleRequest = (request, response) => {
             renderContent("./about.html", response);
             break;
         case "/general":
-            renderContent("./general.txt", response);
+            renderContent("./general.html", response);
         break;
         case "/superhero":
             renderContent("./superhero.html", response);
             break;
         default: 
-            response.write("Oops something went wrong! Route not defined.");
+            response.write("Oops! This route is not defined Here's a joke to entertain you instead: Why did Batman and Robin never fight crime on April 1st? ............................................. Because the Joker would take it too seriously!");
             response.end();
     }
 }
