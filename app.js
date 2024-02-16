2943dc73b4f6562f412bf81c87f327852158c3a2
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-16 10:22:36 +0100

    new-setup

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
-const http = require("http");
-const fs = require ("fs");
-const port = 3000;
+var url = require("url");
+var fs = require("fs");
 
-const senErrorMessage = (res, statusCode, message) => {
-    res.writeHead(statusCode, {"Content-Type": "text/plain"});
-    res.end(message);
+
+
+const renderHTML = (path, response) => {
+    fs.readFile(path, null, function(error,data) { //might replace null with utf8
+        if (error) {
+            response.writeHead(404);
+            rensponse.write("File not found");
+        } else {
+            response.write(data);
+        }
+        rensponse.end();
+    });
 }
 
-const sendHtmlResponse = (res, data) => {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(data);
-    res.end();
-};
+const handleRequest = (request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html"});
 
-const server = http.createServer(function(req, res) {
-    if(req.url.includes("/home")) {
-        fs.readFile("./index.html", function(error, data) {
-            if(error) {
-                senErrorMessage(res, 404, "Error: Page not found")
-            } else {
-                sendHtmlResponse(res, data)
-            }
-        });
-    } else if(req.url.includes("/about")) {
-        fs.readFile("./about.html", function(error, data) {
-            if(error) {
-                senErrorMessage(res, 404, "Error: Page not found")
-            } else {
-                sendHtmlResponse(res, data)
-            }
-        });  
-    } else if(req.url.includes("/superhero")) {
-        fs.readFile("./superhero.html", function(error, data) {
-            if(error) {
-                senErrorMessage(res, 404, "Error: Page not found")
-            } else {
-                sendHtmlResponse(res, data)
-            }
-        });  
-    } else {
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.end("Oh no! There is an error: Page not found")
+    var path = new URL (request.url).pathname;
+    switch (path) {
+        case "/":
+            renderHTML("./index.html", response);
+            break;
+        case "/about":
+            renderHTML("./about.html", response);
+            break;
+        default: 
+            response.writeHead(404);
+            response.write("Route not defined");
+            response.end();
     }
-});
+}
 
-server.listen(port, function(error) {
-    if (error) {
-        console.log("Oops, something went wrong!", error)
-    } else {
-        console.log("Server listening on port " + port)
-    }
-})
+module.exports = {
+    handleRequest
+}
+
+
+// module.exports = {
+//     handleRequest: function(request, response) {
+//         response.writeHead(200, {"Content-Type": "text/html"});
+    
+//         var path = url.parse(request.url).pathname;
+//         switch (path) {
+//             case "/":  
+//         }
+//     }
+// };
